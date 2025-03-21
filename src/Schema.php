46b4199411781b2e304d73254b6f5fecc5b0bc69
Kevin Hill <loneregister01@gmail.com>
2025-03-21 10:41:09 -0700

    Update Schema.php
    Move constant DEFAULT_MAX_NEST to a static constant from the context object

--- src/Schema.php
+++ src/Schema.php
@@ -37,8 +37,6 @@ class Schema extends JsonSchema implements MetaHolder, SchemaContract, HasDefaul
     const CONST_PROPERTY = 'const';
     const DEFAULT_PROPERTY = 'default';
 
-    const DEFAULT_MAX_NEST = 200;
-
     const DEFAULT_MAPPING = 'default';
 
     const VERSION_AUTO = 'a';
@@ -167,7 +165,7 @@ class Schema extends JsonSchema implements MetaHolder, SchemaContract, HasDefaul
         $options->import = true;
 
         if ($options->refResolver === null) {
-            $options->refResolver = new RefResolver($data, $options->maxNestLevel ?? DEFAULT_MAX_NEST);
+            $options->refResolver = new RefResolver($data, $options->maxNestLevel ?? Context::DEFAULT_MAX_NEST);
         } else {
             $options->refResolver->setRootData($data);
         }
